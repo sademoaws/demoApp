6e285e16aecea64e696fe638ca9fc888a7a08fcb
d42n81 <dmoore42n81@gmail.com>
2022-08-03 22:52:35 -0400

    Added Cypress

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,36 @@
+// import '@axe-devtools/cypress';
+describe('ABCD Tech Home', () => {
+
+    before(() => {
+      cy.visit('https://d42n81.github.io/demoApp/')
+    });
+
+    it('Should have main navigation opened', () => {
+      cy.get('.card-header')
+      .contains('Hello')
+    });
+
+
+
+    /** 
+     * Axe Ruleset - running analysis against specific ruleset(s)
+     * .setAxeRuleset('508' | 'wcag2' | 'wcag2.1')
+     * Default param wcag2
+     * 
+     * Analyzes a page using wcag2.1
+     * Creates a `results` object based on the ruleset
+     * JSON file is create of the results
+     * HTML file is created of the results
+     */
+    it('Should be accessible', () => {
+      // cy.setAxeRuleset('wcag2.1').axeAnalyze({name: "home"}).debug();
+      cy.axeAnalyze({name:"home"});
+      cy.getAxeResults().then(async results => {
+        const resultsDir = './a11y-results/cypress/'
+        await cy.writeFile(`${resultsDir}home.json`, results)
+        await cy.task('reportAsHTML', { resultsDir })
+      })
+    });
+
+
+  })
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,26 +7,53 @@ const Dashboard = () => {
           <Col lg="6">
             <Card className="mb-4">
             <CardHeader>
-                <h5>Hello</h5> 
+                <h5>Product Title</h5>
               </CardHeader>
               <CardBody>
-               
-                <p>world</p>
+               <img src={require("./images/Circle.png")} alt="Circle"/>
+                <p>Product Description</p>
               </CardBody>
             </Card>
-  
-            
-      
-          
-          </Col> 
-  
-          
+
+            <Card className="mb-4">
+            <CardHeader>
+                <h5>Another Product Title</h5>
+              </CardHeader>
+              <CardBody>
+               <img src={require("./images/Square.png")}/>
+                <p aria-role="image">Another Product Description</p>
+                <img src="foo.png"/>
+              </CardBody>
+            </Card>
+
+
+
+          </Col>
+
+          <Col lg="6">
+            <Card className="mb-4">
+            <CardHeader>
+                <h5>A Final Product Title</h5>
+              </CardHeader>
+              <CardBody>
+               <img src={require("./images/Triangle.png")}/>
+                <p>A final Product Description</p>
+              </CardBody>
+            </Card>
+
+
+
+
+
+          </Col>
+
+
         </Row>
       </>
     );
   };
-  
+
   export default Dashboard;
-  
+
 
                   {/* <img src={require('./images/Circle.png')}/> */}
